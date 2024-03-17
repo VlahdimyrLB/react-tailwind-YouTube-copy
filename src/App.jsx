5ed06514c24fa082e9bb6365e78578730f908488
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-03-17 21:44:46 +0800

    Added pre-Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 import Grid from "./Grid";
 
 const App = () => {
   return (
-    <>
+    <div className="relative">
       <Navbar />;
-      <Grid />;
-    </>
+      <div className="md:ml-20">
+        <Grid />;
+      </div>
+      <Sidebar />;
+    </div>
   );
 };
 export default App;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,8 @@
+const Sidebar = () => {
+  return (
+    <aside className="w-20 top-14 left-0 right-0 fixed h-screen z-10 bg-gray-300 hidden md:block">
+      <div>Side Nav Here</div>
+    </aside>
+  );
+};
+export default Sidebar;
